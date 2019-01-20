8e2dfdd606550bf4af3f1f24d55629f5f30e3abc
mrtillman <askmrtillman@gmail.com>
2019-01-20 10:26:37 -0500

    add dev branch; implement grav.exists

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function Grav(email, password){
   this.api_url = `https://secure.gravatar.com/xmlrpc?user=${this.hash}`;
 }
 
-Grav.prototype.test = function(){
+Grav.prototype.exists = function(){
   return new Promise((resolve, reject) => {
-    const payload = this.xml.grav_test();
+    const payload = this.xml.grav_exists(this.hash);
     const result = api.get(this.api_url, payload);
     resolve(result);
   })
@@ -37,6 +37,14 @@ Grav.prototype.saveUrl = function(imageUrl){
 Grav.prototype.useUserimage = function(imageName){
   return new Promise((resolve, reject) => {
     const payload = this.xml.grav_useUserimage(imageName);
+    const result = api.get(this.api_url, payload);
+    resolve(result);
+  })
+}
+
+Grav.prototype.test = function(){
+  return new Promise((resolve, reject) => {
+    const payload = this.xml.grav_test();
     const result = api.get(this.api_url, payload);
     resolve(result);
   })
