74795d03b1fe9d1a55134fe073de449d774973b0
mrtillman <askmrtillman@gmail.com>
2019-02-02 20:17:37 -0500

    add unit tests

--- spec/lib/grav.api.spec.js
+++ spec/lib/grav.api.spec.js
@@ -43,6 +43,101 @@ describe('xml-rpc api client', function(){
 
   })
 
+  it('should implement grav.addresses', function(){
+
+    nock(utils.api_origin)
+    .get(`/xmlrpc?user=${hash}`)
+    .reply(200, response);
+    
+    payload = xml.grav_addresses(hash);
+    
+    api.get(endpoint, payload)
+       .then(response => {
+          const _response = JSON.parse(response);
+          expect(typeof response).toBe('string');
+          expect(typeof _response).toBe('object');
+          expect(_response._declaration).toBeDefined();
+          expect(_response.methodResponse).toBeDefined();
+        })
+
+  })
+
+  it('should implement grav.userImages', function(){
+
+    nock(utils.api_origin)
+    .get(`/xmlrpc?user=${hash}`)
+    .reply(200, response);
+    
+    payload = xml.grav_userImages(hash);
+    
+    api.get(endpoint, payload)
+       .then(response => {
+          const _response = JSON.parse(response);
+          expect(typeof response).toBe('string');
+          expect(typeof _response).toBe('object');
+          expect(_response._declaration).toBeDefined();
+          expect(_response.methodResponse).toBeDefined();
+        })
+
+  })
+
+  it('should implement grav.saveUrl', function(){
+
+    nock(utils.api_origin)
+    .get(`/xmlrpc?user=${hash}`)
+    .reply(200, response);
+    
+    payload = xml.grav_saveUrl(hash);
+    
+    api.get(endpoint, payload)
+       .then(response => {
+          const _response = JSON.parse(response);
+          expect(typeof response).toBe('string');
+          expect(typeof _response).toBe('object');
+          expect(_response._declaration).toBeDefined();
+          expect(_response.methodResponse).toBeDefined();
+        })
+
+  })
+
+  it('should implement grav.removeImage', function(){
+
+    nock(utils.api_origin)
+    .get(`/xmlrpc?user=${hash}`)
+    .reply(200, response);
+    
+    payload = xml.grav_removeImage(hash);
+    
+    api.get(endpoint, payload)
+       .then(response => {
+          const _response = JSON.parse(response);
+          expect(typeof response).toBe('string');
+          expect(typeof _response).toBe('object');
+          expect(_response._declaration).toBeDefined();
+          expect(_response.methodResponse).toBeDefined();
+        })
+
+  })
+
+  it('should implement grav.deleteUserImage', function(){
+
+    nock(utils.api_origin)
+    .get(`/xmlrpc?user=${hash}`)
+    .reply(200, response);
+    
+    payload = xml.grav_deleteUserImage(hash);
+    
+    api.get(endpoint, payload)
+       .then(response => {
+          const _response = JSON.parse(response);
+          expect(typeof response).toBe('string');
+          expect(typeof _response).toBe('object');
+          expect(_response._declaration).toBeDefined();
+          expect(_response.methodResponse).toBeDefined();
+        })
+
+  })
+
   it('should implement grav.test', function(){
 
     nock(utils.api_origin)
