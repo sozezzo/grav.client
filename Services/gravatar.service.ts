640fa97ed0a660d56015c805a317dd8ec1358f58
mrtillman <askmrtillman@gmail.com>
2019-11-20 02:07:53 -0500

    implement grav.test and grav.exists

--- Services/gravatar.service.ts
+++ Services/gravatar.service.ts
@@ -1,22 +1,44 @@
 import { Md5 } from 'ts-md5/dist/md5';
 import { HttpShim } from '../Infrastructure/http-shim';
+import { XmlService } from './xml.service';
+import { xml2js } from 'xml-js';
 
 export class GravatarService {
 
-  protected hash : string | Int32Array;
+  protected hash : string;
   protected origin : string = "https://secure.gravatar.com";
   protected endpoint : string;
   protected _password : string;
+
   public http: HttpShim;
+  public xml: XmlService;
 
   constructor(public email: string,
               password: string) {
     const _email = `${email}`.trim().toLowerCase();
-    this.hash = Md5.hashStr(_email);
+    this.hash = Md5.hashStr(_email).toString();
     this.endpoint = `${this.origin}/xmlrpc?user=${this.hash}`;
     this._password = password;
   }
-  test(): string {
-    return this.hash.toString();
+  public async exists() : Promise<object> {
+    const message = this.xml.exists(this.hash, this._password);
+    const response = await this.http.rpc(message);
+    // TODO: incorporate result class
+    if(response.ok){
+      const data = await response.text();
+      return xml2js(data);
+    } else {
+      return {};
+    }
+  }
+  public async test() : Promise<object> {
+    const message = this.xml.test(this._password);
+    const response = await this.http.rpc(message);
+    if(response.ok){
+      const data = await response.text();
+      return xml2js(data);
+    } else {
+      return {};
+    }
   }
 };
