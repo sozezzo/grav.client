2c5e1cdf2d99d8e965f21dbd8aae6e5e91b47650
mrtillman <askmrtillman@gmail.com>
2019-11-20 02:07:07 -0500

    connect xml service

--- Presentation/index.ts
+++ Presentation/index.ts
@@ -1,9 +1,12 @@
 import { GravatarService } from '../Services/gravatar.service';
 import { HttpShim } from '../Infrastructure/http-shim';
+import { XmlService } from '../Services/xml.service';
 
 export class GravatarClient extends GravatarService {
   constructor(public email: string, password: string){
     super(email, password);
+    this.xml = new XmlService();
     this.http = new HttpShim();
+    this.http.endpoint = this.endpoint;
   }
 }
