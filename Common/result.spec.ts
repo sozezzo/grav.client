b8196c6d37651196c2d8a4c7b367f1618adee65b
mrtillman <askmrtillman@gmail.com>
2019-11-19 23:54:47 -0500

    scaffold common layer

---
+++ Common/result.spec.ts
@@ -0,0 +1,11 @@
+import Result from './result';
+
+describe('Result', () => {
+  let result: Result<Boolean>;
+  beforeEach(() => {
+    result = Result.Ok<Boolean>(true);
+  })
+  it('should work', () => {
+    expect(result.DidSucceed).toBe(true);
+  })
+})
---
+++ Common/result.ts
@@ -0,0 +1,37 @@
+export default class Result<T> {
+  constructor(DidSucceed: Boolean, ErrorMessage: string = ""){
+    this.didSucceed = DidSucceed;
+    this.didFail = !DidSucceed;
+    this.errorMessage = ErrorMessage;
+  }
+
+  private didSucceed: Boolean;
+  public get DidSucceed() : Boolean {
+    return this.didSucceed;
+  }
+
+  private didFail: Boolean;
+  public get DidFail() : Boolean {
+    return this.didFail;
+  }
+
+  private errorMessage: String;
+  public get ErrorMessage() : String {
+    return this.errorMessage;
+  }
+
+  private value: T;
+  public get Value() : T {
+    return this.value;
+  }
+
+  public static Ok<T>(Value: T): Result<T> {
+    const result = new Result<T>(true);
+    result.value = Value;
+    return result;
+  }
+
+  public static Fail<T>(ErrorMessage: string): Result<T> {
+    return new Result<T>(false, ErrorMessage);
+  }
+}
