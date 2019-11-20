794b86170f37363f9dd026c4193cf95271c7151f
mrtillman <askmrtillman@gmail.com>
2019-11-20 02:04:31 -0500

    implement rpc method

--- Infrastructure/http-shim.ts
+++ Infrastructure/http-shim.ts
@@ -1,3 +1,14 @@
+import * as fetchImport from 'isomorphic-unfetch';
+
+const fetch = (fetchImport.default || fetchImport) as typeof fetchImport.default;
+
 export class HttpShim {
-  rpc() : any { }
+  public endpoint: string;
+  async rpc(message: string) : Promise<Response> {
+    return await fetch(this.endpoint, {
+      method: "POST",
+      headers: { 'content-type' : 'text/xml' },
+      body:    message
+    });
+  }
 }
