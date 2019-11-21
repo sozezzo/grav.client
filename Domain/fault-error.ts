4532a13e561f43796acdda1df663f9f50e3fb79e
mrtillman <askmrtillman@gmail.com>
2019-11-20 22:42:42 -0500

    catch rpc response fault

--- Domain/fault-error.ts
+++ Domain/fault-error.ts
@@ -1,6 +1,6 @@
 export class FaultError extends Error {
   constructor(public faultCode: number,
               public faultString: string) {
-    super(faultString);
+    super(`[${faultCode}]: ${faultString}`);
   }
 }
--- Domain/method-response.ts
+++ Domain/method-response.ts
@@ -1,6 +1,23 @@
+import { FaultError } from './fault-error';
+
 export class MethodResponse {
 
-  constructor(public json: any) { }
+  constructor(public json: any) { 
+    let faultCode: number = 0;
+    let faultString: string = "";
+    const { fault } = this.json.methodResponse;
+    if(fault){
+      const members : Array<any> = fault.value.struct.member;
+      members.map(member => {
+        if(member.name._text == "faultCode"){
+          faultCode = this.parseFieldValue(member.value);
+        } else if(member.name._text == "faultString"){
+          faultString = this.parseFieldValue(member.value);
+        }
+      })
+      throw new FaultError(faultCode, faultString);
+    }
+  }
 
   protected parseFieldValue<T>(fieldValue: any): T {
     if(fieldValue.boolean) return fieldValue.boolean._text as T;
