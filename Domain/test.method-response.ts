367506dae38bbafe44b83fe22ea09e541e6d6ff2
mrtillman <askmrtillman@gmail.com>
2019-11-20 21:26:46 -0500

    parse name and value

--- Domain/test.method-response.ts
+++ Domain/test.method-response.ts
@@ -2,7 +2,14 @@ import { MethodResponse } from './method-response';
 import { xml2js } from 'xml-js'
 
 export class TestMethodResponse extends MethodResponse {
+
+  public name: string;
+  public value: number;
+
   constructor(public xml: string){
     super(xml2js(xml, { compact: true }));
+    const { name, value } = this.json.methodResponse.params.param.value.struct.member;
+    this.name = this.parseFieldValue(name);
+    this.value = this.parseFieldValue(value);
   }
 }
