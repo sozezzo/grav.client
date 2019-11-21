5a3c51941aa7fad761888a2394f020bd6c362f3d
mrtillman <askmrtillman@gmail.com>
2019-11-20 21:20:31 -0500

    inherit from method response class; fix breaking changes

--- Domain/exists.method-response.ts
+++ Domain/exists.method-response.ts
@@ -1,11 +1,15 @@
 import { MethodResponse } from './method-response';
 import { xml2js } from 'xml-js'
 
-export class ExistsMethodResponse implements MethodResponse {
-  
-  public json: any;
+export class ExistsMethodResponse extends MethodResponse {
+
+  public name: string;
+  public value: number;
 
   constructor(public xml: string){
-    this.json = xml2js(xml);
+    super(xml2js(xml, { compact: true }));
+    const { name, value } = this.json.methodResponse.params.param.value.struct.member;
+    this.name = this.parseFieldValue(name);
+    this.value = this.parseFieldValue(value);
   }
 }
--- Domain/test.method-response.ts
+++ Domain/test.method-response.ts
@@ -1,11 +1,8 @@
 import { MethodResponse } from './method-response';
 import { xml2js } from 'xml-js'
 
-export class TestMethodResponse implements MethodResponse {
-  
-  public json: any;
-
+export class TestMethodResponse extends MethodResponse {
   constructor(public xml: string){
-    this.json = xml2js(xml);
+    super(xml2js(xml, { compact: true }));
   }
 }
