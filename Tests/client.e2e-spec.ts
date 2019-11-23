3aa4515fb9d49239f51a32a0bf28a853a8a8d796
mrtillman <askmrtillman@gmail.com>
2019-11-23 13:30:13 -0500

    add e2e test for grav.exists

--- Tests/client.e2e-spec.ts
+++ Tests/client.e2e-spec.ts
@@ -1,7 +1,18 @@
 import { GravatarClient } from '../Presentation';
+import { config } from 'dotenv';
+
+config({ path: 'Tests/.env' });
+
+const email = process.env.EMAIL as string;
+const password = process.env.PASSWORD as string;
 
 describe('GravatarClient', () => {
-  it('should work', () => {
-    expect(true).toBe(true);
+  let client: GravatarClient;
+  beforeEach(() => {
+    client = new GravatarClient(email, password);
+  })
+  it('should invoke grav.exists', async () => {
+    const result = await client.exists();
+    expect(result.DidSucceed).toBe(true);
   })
 })
