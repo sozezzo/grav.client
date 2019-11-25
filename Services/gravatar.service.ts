2a92779e1fbe6e24bbd2f20dfadbdf603d875387
mrtillman <askmrtillman@gmail.com>
2019-11-24 23:47:44 -0500

    implement method for grav.addresses

--- Services/gravatar.service.ts
+++ Services/gravatar.service.ts
@@ -1,11 +1,14 @@
 import { Md5 } from 'ts-md5/dist/md5';
 import { HttpShim } from '../Infrastructure/http-shim';
+import { Result } from '../Common/result';
+
+import { AddressesMethodCall } from '../Domain/addresses.method-call';
 import { ExistsMethodCall } from '../Domain/exists.method-call';
 import { TestMethodCall } from '../Domain/test.method-call';
-import { ExistsMethodResponse } from '../Domain/exists.method-response';
-import { Result } from '../Common/result';
-import { TestMethodResponse } from '../Domain/test.method-response';
+
 import { AddressesMethodResponse } from '../Domain/addresses.method-response';
+import { ExistsMethodResponse } from '../Domain/exists.method-response';
+import { TestMethodResponse } from '../Domain/test.method-response';
 
 export class GravatarService {
 
@@ -25,6 +28,17 @@ export class GravatarService {
     if(response.ok){
       const xmlRes = await response.text();
       const methodResponse = new ExistsMethodResponse(xmlRes);
+      return Result.Ok(methodResponse);
+    } else {
+      return Result.Fail(response.statusText);
+    }
+  }
+  public async addresses() : Promise<Result<AddressesMethodResponse>> {
+    const methodCall = new AddressesMethodCall(this._password);
+    const response = await this.http.rpc(methodCall.xml);
+    if(response.ok){
+      const xmlRes = await response.text();
+      const methodResponse = new AddressesMethodResponse(xmlRes);
       return Result.Ok(methodResponse);
     } else {
       return Result.Fail(response.statusText);
