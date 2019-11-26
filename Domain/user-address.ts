c1bcac37ee1e29b72501bd05bdb77d57455e4cea
mrtillman <askmrtillman@gmail.com>
2019-11-26 14:28:48 -0500

    add user address

---
+++ Domain/user-address.ts
@@ -0,0 +1,8 @@
+import { ImageRating } from './image-rating';
+
+export class UserAddress {
+  public imageRating: ImageRating;
+  public userEmail: string;
+  public userImage: string;
+  public userImageUrl: string;
+}
