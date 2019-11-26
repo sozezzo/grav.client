2bbc459c9336e13b7acd854dc116b824ea30a57e
mrtillman <askmrtillman@gmail.com>
2019-11-26 15:22:17 -0500

    export image rating enum

--- Presentation/index.ts
+++ Presentation/index.ts
@@ -1,6 +1,8 @@
 import { GravatarService } from '../Services/gravatar.service';
 import { HttpShim } from '../Infrastructure/http-shim';
 
+export { ImageRating } from '../Domain/image-rating';
+
 export class GravatarClient extends GravatarService {
   constructor(public email: string, password: string){
     super(email, password);
