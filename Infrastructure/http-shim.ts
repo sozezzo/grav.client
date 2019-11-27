9b3853a81982126a4e5fe7610982bb46269e24ea
mrtillman <askmrtillman@gmail.com>
2019-11-26 19:46:08 -0500

    add method to post image file

--- Infrastructure/http-shim.ts
+++ Infrastructure/http-shim.ts
@@ -1,4 +1,6 @@
 import * as fetchImport from 'isomorphic-unfetch';
+import { createReadStream } from 'fs';
+import FormData from 'isomorphic-form-data';
 
 const fetch = (fetchImport.default || fetchImport) as typeof fetchImport.default;
 
@@ -15,6 +17,15 @@ export class HttpShim {
       method: "POST",
       headers: { 'content-type' : 'text/xml' },
       body: message
+    });
+  }
+
+  async postImageFile(imageFilePath: string) : Promise<Response> {
+    const formData = new FormData();
+    formData.append('avatar', createReadStream(imageFilePath));
+    return await fetch('https://dailyavatar.io/api/v1/avatars', {
+      method: "POST",
+      body: <any>formData
     });
   }
 }
