0fad47b0f58f78f6765d0659249a72782bd357cd
mrtillman <askmrtillman@gmail.com>
2019-11-28 23:25:00 -0500

    finalize e2e tests

--- Tests/client.e2e-spec.ts
+++ Tests/client.e2e-spec.ts
@@ -1,41 +1,66 @@
 import { GravatarClient } from '../Presentation';
 import { config } from 'dotenv';
-import path from 'path';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 config({ path: 'Tests/.env' });
 
+const imgPath = join(__dirname, '../Common/Assets/gump.jpg');
 const email = process.env.EMAIL as string;
 const password = process.env.PASSWORD as string;
+const uploadedImageNames: Array<string> = [];
+const originalPrimaryImage: string = "4ddf23534256fb555cfbf10acd7728b2";
 
 describe('GravatarClient', () => {
+
   let client: GravatarClient;
 
   beforeEach(() => {
     client = new GravatarClient(email, password);
   })
-  
-  it(`should invoke grav.exists`, async () => {
+
+  it('should invoke grav.exists', async () => {
     const result = await client.exists();
     expect(result.DidSucceed).toBe(true);
   })
-
-  it(`should get account email addresses`, async () => {
+  it('should get account email addresses', async () => {
     const result = await client.addresses();
     expect(result.DidSucceed).toBe(true);
   })
-
-  it(`should get user images`, async () => {
+  it('should get user images', async () => {
     const result = await client.userImages();
     expect(result.DidSucceed).toBe(true);
   })
-
-  it(`should upload image file`, async () => {
-    const imgPath = path.join(__dirname, '../Common/Assets/gump.jpg');
+  it('should upload image file', async () => {
     const result = await client.saveImage(imgPath);
+    uploadedImageNames.push(result.Value.imageName);
     expect(result.DidSucceed).toBe(true);
   })
-
-  it(`should invoke grav.test`, async () => {
+  it('should upload encoded image', async () => {
+    const bitmap = readFileSync(imgPath);
+    const imageData = new Buffer(bitmap).toString('base64');
+    const result = await client.saveEncodedImage(imageData, 'jpeg');
+    uploadedImageNames.push(result.Value.imageName);
+    expect(result.DidSucceed).toBe(true);
+  })
+  it('should save image url', async () => {
+    const result = await client.saveImageUrl("https://via.placeholder.com/150");
+    uploadedImageNames.push(result.Value.imageName);
+    expect(result.DidSucceed).toBe(true);
+  })
+  it('should remove primary image', async () => {
+    const result = await client.removeImage();
+    expect(result.DidSucceed).toBe(true);
+  })
+  it('should update primary image', async () => {
+    const result = await client.useUserImage(originalPrimaryImage);
+    expect(result.DidSucceed).toBe(true);
+  })
+  it('should delete image', async () => {
+    const result = await client.deleteUserImage(uploadedImageNames[0]);
+    expect(result.DidSucceed).toBe(true);
+  })
+  it('should invoke grav.test', async () => {
     const result = await client.test();
     expect(result.DidSucceed).toBe(true);
   })
