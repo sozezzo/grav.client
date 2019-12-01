0649790625f84a8058196151195b1b4d52b31994
mrtillman <askmrtillman@gmail.com>
2019-11-30 21:02:19 -0500

    add image  file path + email hash

--- Common/TestDoubles/primitive-stubs.ts
+++ Common/TestDoubles/primitive-stubs.ts
@@ -1,2 +1,4 @@
 export const email = "peter.parker@example.com";
+export const emailHash = "1a9e97710a2477b1040d332e9e815cca";
 export const password = "123";
+export const imageFilePath = "no/one/here.png";
