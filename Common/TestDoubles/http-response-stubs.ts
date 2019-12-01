7e33ebcbb1c8f79254a8b2a2c37688054f4e031a
mrtillman <askmrtillman@gmail.com>
2019-12-01 00:08:24 -0500

    mock grav.addresses http response

--- Common/TestDoubles/http-response-stubs.ts
+++ Common/TestDoubles/http-response-stubs.ts
@@ -30,6 +30,56 @@ export class ExistsHttpResponseStub {
   }
 }
 
+export class AddressesHttpResponseStub {
+  private xml: string;
+  constructor(success: boolean, email: string, errorMessage: string = ""){
+    this.xml = success ? `
+    <?xml version="1.0"?>
+    <methodResponse>
+        <params>
+            <param>
+                <value>
+                    <struct>
+                        <member>
+                            <name>${email}</name>
+                            <value>
+                                <struct>
+                                    <member>
+                                        <name>rating</name>
+                                        <value>
+                                            <int>0</int>
+                                        </value>
+                                    </member>
+                                    <member>
+                                        <name>userimage</name>
+                                        <value>
+                                            <string>4ddf23534256fb555cfbf10acd7728b2</string>
+                                        </value>
+                                    </member>
+                                    <member>
+                                        <name>userimage_url</name>
+                                        <value>
+                                            <string>http://en.gravatar.com/userimage/150849239/4ddf23534256fb555cfbf10acd7728b2.jpg</string>
+                                        </value>
+                                    </member>
+                                </struct>
+                            </value>
+                        </member>
+                    </struct>
+                </value>
+            </param>
+        </params>
+    </methodResponse>
+  ` : rpcFaultXml(errorMessage);
+  }
+  public get value(): Promise<Response> {
+    return Promise.resolve({
+      ok: true,
+      status: 200,
+      text: () => Promise.resolve(this.xml)
+    } as Response);
+  }
+}
 
 function rpcFaultXml(faultString: string){
   return `
