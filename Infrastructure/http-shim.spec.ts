f681eac50f5fcb1283eff255f0f994695873caef
mrtillman <askmrtillman@gmail.com>
2019-12-01 00:09:01 -0500

    remove fetch mock

--- Infrastructure/http-shim.spec.ts
+++ Infrastructure/http-shim.spec.ts
@@ -2,17 +2,15 @@ import { HttpShim } from './http-shim';
 import { emailHash } from '../Common/TestDoubles/primitive-stubs';
 import { join } from 'path';
 import { readFileSync } from 'fs';
-import fetchMock from 'fetch-mock';
 
 describe('HttpShim', () => {
+  
   let http : HttpShim;
-  const imagePath = 
+
   beforeAll(() => {
     http = new HttpShim(emailHash);
-    const origin = "https://secure.gravatar.com";
-    fetchMock.get(`${origin}/xmlrpc?user=${emailHash}`, 200)
-             .post('https://dailyavatar.io/api/v1/avatars', 200);
   })
+
   it('should have endpoint', () => {
     expect(http.endpoint).toBeDefined();
   })
