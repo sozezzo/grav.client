82e23a7e2c0c6342b51701d7d46dae8101c8d318
mrtillman <askmrtillman@gmail.com>
2019-12-01 18:20:49 -0500

    simplify imports; fix breaking changes

--- Services/gravatar.service.spec.ts
+++ Services/gravatar.service.spec.ts
@@ -1,21 +1,17 @@
-import { GravatarService } from './gravatar.service';
-import { HttpShim } from '../Infrastructure/http-shim';
-
-import { 
-  ResponseStub, ExistsHttpResponseStub, AddressesHttpResponseStub,
-  UserImagesHttpResponseStub, SaveImageHttpResponseStub, 
-  SaveEncodedImageHttpResponseStub, SaveImageUrlHttpResponseStub, UseUserImageHttpResponseStub, RemoveImageHttpResponseStub, DeleteUserImageHttpResponseStub, TestHttpResponseStub
-} from '../Common/TestDoubles/http-response-stubs';
-
-import { email, emailHash, password, imageUrl } from '../Common/TestDoubles/primitive-stubs';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+
+import { GravatarService } from './gravatar.service';
+import { HttpShim } from '../Infrastructure/http-shim';
+import { email, password, imageUrl } from '../Common/TestDoubles/primitive-stubs';
+
+import * as Stub from '../Common/TestDoubles/http-response-stubs';
 
 describe('GravatarService', () => {
   
   let service : GravatarService;
 
-  function mockHttpShim(responseStub: ResponseStub) : HttpShim {
+  function mockHttpShim(responseStub: Stub.ResponseStub) : HttpShim {
     const httpShim = new HttpShim(service.emailHash);
     spyOn(httpShim, 'rpc').and.returnValue(responseStub.value);
     return httpShim;
@@ -26,7 +22,7 @@ describe('GravatarService', () => {
   })
 
   it('should check if account exists', async () => {
-    const responseStub = new ExistsHttpResponseStub(true, service.emailHash);
+    const responseStub = new Stub.ExistsHttpResponseStub(true, service.emailHash);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.exists();
@@ -38,21 +34,21 @@ describe('GravatarService', () => {
     expect(rgx.test(service.gravatarImageUrl)).toBeTrue();
   })
   it('should get user account email addresses', async () => {
-    const responseStub = new AddressesHttpResponseStub(true, service.emailHash);
+    const responseStub = new Stub.AddressesHttpResponseStub(true, service.emailHash);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.addresses();
     expect(result.DidSucceed).toBeTrue();
   })
   it('should get user images', async () => {
-    const responseStub = new UserImagesHttpResponseStub(true, service.emailHash);
+    const responseStub = new Stub.UserImagesHttpResponseStub(true, service.emailHash);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.userImages();
     expect(result.DidSucceed).toBeTrue();
   })
   it('should save image file', async () => {
-    const responseStub = new SaveImageHttpResponseStub(true);
+    const responseStub = new Stub.SaveImageHttpResponseStub(true);
     const httpShim = mockHttpShim(responseStub);
     const imageFilePath = join(__dirname, "../Common/Assets/bubba.jpg");
     service.http = httpShim;
@@ -60,7 +56,7 @@ describe('GravatarService', () => {
     expect(result.DidSucceed).toBeTrue();
   })
   it('should save encoded image', async () => {
-    const responseStub = new SaveEncodedImageHttpResponseStub(true);
+    const responseStub = new Stub.SaveEncodedImageHttpResponseStub(true);
     const httpShim = mockHttpShim(responseStub);
     const imgPath = join(__dirname, '../Common/Assets/gump.jpg');
     const bitmap = readFileSync(imgPath);
@@ -70,39 +66,38 @@ describe('GravatarService', () => {
     expect(result.DidSucceed).toBeTrue();
   })
   it('should save image url', async () => {
-    const responseStub = new SaveImageUrlHttpResponseStub(true);
+    const responseStub = new Stub.SaveImageUrlHttpResponseStub(true);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.saveImageUrl(imageUrl);
     expect(result.DidSucceed).toBeTrue();
   })
   it('should use user image', async () => {
-    const responseStub = new UseUserImageHttpResponseStub(true, email);
+    const responseStub = new Stub.UseUserImageHttpResponseStub(true, email);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.saveImageUrl(imageUrl);
     expect(result.DidSucceed).toBeTrue();
   })
   it('should remove image', async () => {
-    const responseStub = new RemoveImageHttpResponseStub(true, email);
+    const responseStub = new Stub.RemoveImageHttpResponseStub(true, email);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.saveImageUrl(imageUrl);
     expect(result.DidSucceed).toBeTrue();
   })
   it('should delete user image', async () => {
-    const responseStub = new DeleteUserImageHttpResponseStub(true);
+    const responseStub = new Stub.DeleteUserImageHttpResponseStub(true);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.saveImageUrl(imageUrl);
     expect(result.DidSucceed).toBeTrue();
   })
   it('should do sanity check', async () => {
-    const responseStub = new TestHttpResponseStub(true);
+    const responseStub = new Stub.TestHttpResponseStub(true);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.saveImageUrl(imageUrl);
     expect(result.DidSucceed).toBeTrue();
   })
-
 })
