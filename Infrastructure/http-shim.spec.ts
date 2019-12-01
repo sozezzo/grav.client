df81421aa3a4b08e36827b5827e0047f68be60e6
mrtillman <askmrtillman@gmail.com>
2019-11-30 22:36:15 -0500

    finalize tests for http-shim

--- Infrastructure/http-shim.spec.ts
+++ Infrastructure/http-shim.spec.ts
@@ -1,11 +1,35 @@
 import { HttpShim } from './http-shim';
+import { emailHash } from '../Common/TestDoubles/primitive-stubs';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import fetchMock from 'fetch-mock';
 
 describe('HttpShim', () => {
   let http : HttpShim;
-  beforeEach(() => {
-    http = new HttpShim("");
+  const imagePath = 
+  beforeAll(() => {
+    http = new HttpShim(emailHash);
+    const origin = "https://secure.gravatar.com";
+    fetchMock.get(`${origin}/xmlrpc?user=${emailHash}`, 200)
+             .post('https://dailyavatar.io/api/v1/avatars', 200);
   })
-  it('should work', () => {
-    expect(http.rpc).toBeDefined();
+  it('should have endpoint', () => {
+    expect(http.endpoint).toBeDefined();
+  })
+  it('should make rpc call', async () => {
+    const response = await http.rpc("");
+    expect(response.status).toBe(200);
+  })
+  it('should post image file', async () => {
+    const imageFilePath = join(__dirname, "../Common/Assets/gump.jpg");
+    const response = await http.postImageFile(imageFilePath);
+    expect(response.status).toBe(200);
+  })
+  it('should post encoded image', async () => {
+    const imgPath = join(__dirname, '../Common/Assets/gump.jpg');
+    const bitmap = readFileSync(imgPath);
+    const imageData = new Buffer(bitmap).toString('base64');
+    const response = await http.postEncodedImage(imageData, "jpeg");
+    expect(response.status).toBe(200);
   })
 })
