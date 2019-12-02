b11ca376986f484a73a29f5c168a9e4fd6bb3507
mrtillman <askmrtillman@gmail.com>
2019-12-01 21:05:02 -0500

    add usecase to verify email list

---
+++ Application/verify-email-list.use-case.ts
@@ -0,0 +1,14 @@
+import { UseCase } from './use-case.interface';
+import { GravatarClient } from '../Presentation'
+
+export class VerifyEmailListUseCase implements UseCase<{[emailHash: string]: boolean}> {
+
+  public client: GravatarClient;
+  
+  public emailAddresses: Array<string>;
+
+  async execute(): Promise<{[emailHash: string]: boolean}> {
+    const result = await this.client.exists(...this.emailAddresses)
+    return result.Value.exists
+  }
+}
