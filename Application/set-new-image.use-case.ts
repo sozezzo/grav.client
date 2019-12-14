b65a3a092958c4f25ef01facc94ae432149f19ca
mrtillman <askmrtillman@gmail.com>
2019-12-13 20:45:25 -0500

    include image rating

--- Application/set-new-image.use-case.ts
+++ Application/set-new-image.use-case.ts
@@ -1,14 +1,15 @@
 import { UseCase } from './use-case.interface';
-import { GravatarClient } from '../Presentation';
+import { GravatarClient, ImageRating } from '../Presentation';
 
 export class SetNewImageUseCase implements UseCase<string> {
 
   public client: GravatarClient;
   
   public imagePath: string;
+  public imageRating: ImageRating;
 
   public execute(): Promise<string> {
-    return this.client.saveImage(this.imagePath)
+    return this.client.saveImage(this.imagePath, this.imageRating)
               .then(result => result.Value.imageName)
               .then(imageName => {
                 this.client.useUserImage(imageName);
