cc139b32cf61e04f2127be584d4a718b4eecd1c9
mrtillman <askmrtillman@gmail.com>
2019-12-14 16:35:06 -0500

    mock client; test execute method

--- Application/get-primary-image.use-case.spec.ts
+++ Application/get-primary-image.use-case.spec.ts
@@ -1,7 +1,17 @@
-// import {  } from './';
+import { GetPrimaryImageUseCase } from './get-primary-image.use-case';
+import { mockClient } from '../Common/TestDoubles/mock-factory';
+import { UseCaseType } from '../Common/use-case-type';
 
-describe('module',() => {
-  it('should work', () => {
-    expect(true).toBe(true);
+describe('GetPrimaryImageUseCase',() => {
+  let useCase: GetPrimaryImageUseCase;
+
+  beforeAll(() => {
+    useCase = new GetPrimaryImageUseCase();
+    useCase.client = mockClient(UseCaseType.GetPrimaryImage);
+  })
+
+  it('should work', async () => {
+    const primaryImageName = await useCase.execute();
+    expect(primaryImageName).toBeDefined();
   })
 })
