0cdcb1e58c18b73ed0de5e8a92670e9f939197ce
mrtillman <askmrtillman@gmail.com>
2019-12-15 11:35:43 -0500

    test execute method

--- Application/set-new-image.use-case.spec.ts
+++ Application/set-new-image.use-case.spec.ts
@@ -1,7 +1,21 @@
-// import {  } from './';
+import { SetNewImageUseCase } from './set-new-image.use-case';
+import { mockClient } from '../Common/TestDoubles/mock-factory';
+import { UseCaseType } from '../Common/use-case-type';
+import { imageFilePath } from '../Common/TestDoubles/Stubs/primitive-stubs';
+import { ImageRating } from '../Presentation';
 
-describe('module',() => {
-  it('should work', () => {
-    expect(true).toBe(true);
+describe('SetNewImageUseCase',() => {
+  let useCase: SetNewImageUseCase;
+
+  beforeAll(() => {
+    useCase = new SetNewImageUseCase();
+    useCase.client = mockClient(UseCaseType.SetNewImage);
+    useCase.imagePath = imageFilePath;
+    useCase.imageRating = ImageRating.G;
+  })
+
+  it('should work', async () => {
+    const newImageName = await useCase.execute();
+    expect(newImageName).toBeDefined();
   })
 })
