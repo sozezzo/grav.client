318a18227b1738cb2a7bc096c137ee110e66bede
mrtillman <askmrtillman@gmail.com>
2019-12-15 11:30:59 -0500

    stub save-image-url-result

--- Common/TestDoubles/Stubs/result-stubs.ts
+++ Common/TestDoubles/Stubs/result-stubs.ts
@@ -2,6 +2,7 @@ import {
   ExistsMethodResponse,
   AddressesMethodResponse,
   UserImagesMethodResponse,
+  SaveImageUrlMethodResponse,
   UseUserImageMethodResponse,
   TestMethodResponse 
 } from "../../../Domain/method-responses";
@@ -9,6 +10,7 @@ import {
 import * as existsJsonResponse from '../JsonResponses/grav.exists.json';
 import * as addressesJsonResponse from '../JsonResponses/grav.addresses.json';
 import * as userImagesJsonResponse from '../JsonResponses/grav.userImages.json';
+import * as saveImageUrlResponse from '../JsonResponses/grav.saveUrl.json';
 import * as useUserImageJsonResponse from '../JsonResponses/grav.useUserImage.json';
 import * as testJsonResponse from '../JsonResponses/grav.test.json';
 
@@ -31,6 +33,13 @@ export function AddressesResult(useSuccess: boolean){
 export function UserImagesResult(useSuccess: boolean){
   const response = new UserImagesMethodResponse("");
   response.json = userImagesJsonResponse;
+  response.parseMembers();
+  return Promise.resolve(Result.Ok(response));
+}
+
+export function SaveImageUrlResult(useSuccess: boolean){
+  const response = new SaveImageUrlMethodResponse("");
+  response.json = saveImageUrlResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
