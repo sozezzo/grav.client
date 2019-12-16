4a021870dfad86cdcf1a94ac7f75f1b242e4a275
mrtillman <askmrtillman@gmail.com>
2019-12-15 22:01:00 -0500

    test execute method

--- Application/verify-email-list.use-case.spec.ts
+++ Application/verify-email-list.use-case.spec.ts
@@ -1,7 +1,23 @@
-// import {  } from './';
+import { VerifyEmailListUseCase } from './verify-email-list.use-case';
+import { mockClient } from '../Common/TestDoubles/mock-factory';
+import { UseCaseType } from '../Common/use-case-type';
+import {
+  email, emailHash, email2, email2Hash
+} from '../Common/TestDoubles/Stubs/primitive-stubs';
 
-describe('module',() => {
-  it('should work', () => {
-    expect(true).toBe(true);
+describe('VerifyEmailListUseCase', () => {
+  
+  let useCase: VerifyEmailListUseCase;
+
+  beforeAll(() => {
+    useCase = new VerifyEmailListUseCase();    
+    useCase.client = mockClient(UseCaseType.VerifyEmailList);
+  })
+
+  it('should work', async () => {
+    useCase.emailAddresses = [email, email2];
+    const exists = await useCase.execute();
+    expect(exists[emailHash]).toBeDefined();
+    expect(exists[email2Hash]).toBeDefined();
   })
 })
