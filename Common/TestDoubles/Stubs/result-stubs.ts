c53a1ecd3e59710bc5113c96717d31a548a83334
mrtillman <askmrtillman@gmail.com>
2019-12-15 21:43:03 -0500

    fix imports + breaking changes

--- Common/TestDoubles/Stubs/result-stubs.ts
+++ Common/TestDoubles/Stubs/result-stubs.ts
@@ -7,53 +7,48 @@ import {
   TestMethodResponse 
 } from "../../../Domain/method-responses";
 
-import * as existsJsonResponse from '../JsonResponses/grav.exists.json';
-import * as addressesJsonResponse from '../JsonResponses/grav.addresses.json';
-import * as userImagesJsonResponse from '../JsonResponses/grav.userImages.json';
-import * as saveImageUrlResponse from '../JsonResponses/grav.saveUrl.json';
-import * as useUserImageJsonResponse from '../JsonResponses/grav.useUserImage.json';
-import * as testJsonResponse from '../JsonResponses/grav.test.json';
+import * as stub from '../Stubs/json-response-stubs';
 
 import { Result } from "../../result";
 
 export function ExistsResult(useSuccess: boolean){
   const response = new ExistsMethodResponse("");
-  response.json = existsJsonResponse;
+  response.json = stub.existsJsonResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
 
 export function AddressesResult(useSuccess: boolean){
   const response = new AddressesMethodResponse("");
-  response.json = addressesJsonResponse;
+  response.json = stub.addressesJsonResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
 
 export function UserImagesResult(useSuccess: boolean){
   const response = new UserImagesMethodResponse("");
-  response.json = userImagesJsonResponse;
+  response.json = stub.userImagesJsonResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
 
 export function SaveImageUrlResult(useSuccess: boolean){
   const response = new SaveImageUrlMethodResponse("");
-  response.json = saveImageUrlResponse;
+  response.json = stub.saveImageUrlResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
 
 export function UseUserImageResult(useSuccess: boolean){
   const response = new UseUserImageMethodResponse("");
-  response.json = useUserImageJsonResponse;
+  response.json = stub.useUserImageJsonResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
 
 export function TestResult(useSuccess: boolean){
   const response = new TestMethodResponse("");
-  response.json = testJsonResponse;
+  response.json = stub.testJsonResponse;
   response.parseMembers();
   return Promise.resolve(Result.Ok(response));
 }
