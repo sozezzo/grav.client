244f61a103bf2417975985f504506bdbb57fb201
mrtillman <askmrtillman@gmail.com>
2019-12-20 22:28:03 -0500

    test that http shim is defined

--- Presentation/index.spec.ts
+++ Presentation/index.spec.ts
@@ -6,6 +6,9 @@ describe("GravatarClient", function() {
   beforeEach(() => {
     client = new GravatarClient(email, password);
   });
+  it("should have http shim", function() {
+    expect(client.http).toBeDefined();
+  });
   it("should have email hash", function() {
     expect(client.emailHash).toBeDefined();
   });
