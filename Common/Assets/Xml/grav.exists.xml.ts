dc4dc3483cbd48ce4a58e32b151d281fd3b377a2
mrtillman <askmrtillman@gmail.com>
2019-12-22 17:24:58 -0500

    scaffold xml assets

---
+++ Common/Assets/Xml/grav.exists.xml.ts
@@ -0,0 +1,19 @@
+export const existsXmlResponse = `
+<?xml version="1.0"?>
+<methodResponse>
+    <params>
+        <param>
+            <value>
+                <struct>
+                    <member>
+                        <name>{{emailHash}}</name>
+                        <value>
+                            <int>1</int>
+                        </value>
+                    </member>
+                </struct>
+            </value>
+        </param>
+    </params>
+</methodResponse>
+`;
