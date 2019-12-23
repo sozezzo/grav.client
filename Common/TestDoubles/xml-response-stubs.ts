f0de491c60c0ea0d4d9acde206b3843ce0c3a975
mrtillman <askmrtillman@gmail.com>
2019-12-22 20:45:54 -0500

    stub xml for remove-image

--- Common/TestDoubles/xml-response-stubs.ts
+++ Common/TestDoubles/xml-response-stubs.ts
@@ -4,6 +4,8 @@ import { addressesXmlResponse } from "../Assets/Xml/grav.addresses.xml";
 import { addressesMultipleXmlResponse } from "../Assets/Xml/grav.addresses.multiple.xml";
 import { useImageXmlResponse } from "../Assets/Xml/grav.useUserImage.xml";
 import { useImageMultipleXmlResponse } from "../Assets/Xml/grav.useUserImage.multiple.xml";
+import { removeImageXmlResponse } from "../Assets/Xml/grav.removeImage.xml";
+import { removeImageMultipleXmlResponse } from "../Assets/Xml/grav.removeImage.multiple.xml";
 
 export const faultXml = faultXmlResponse;
 export const existsXml = existsXmlResponse;
@@ -11,3 +13,5 @@ export const addressesXml = addressesXmlResponse;
 export const addressesMultipleXml = addressesMultipleXmlResponse;
 export const useImageXml = useImageXmlResponse;
 export const useImageMultipleXml = useImageMultipleXmlResponse;
+export const removeImageXml = removeImageXmlResponse;
+export const removeImageMultipleXml = removeImageMultipleXmlResponse;
