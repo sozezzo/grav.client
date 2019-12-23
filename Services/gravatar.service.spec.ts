8a0dd2d42b752a04140aa45cb942aa9b38cb3d8a
mrtillman <askmrtillman@gmail.com>
2019-12-22 20:16:23 -0500

    add test for multiple addresses

--- Services/gravatar.service.spec.ts
+++ Services/gravatar.service.spec.ts
@@ -5,6 +5,7 @@ import { GravatarService } from "./gravatar.service";
 import { HttpShim } from "../Infrastructure/http-shim";
 import {
   email,
+  email2,
   password,
   imageUrl,
   imageName,
@@ -45,8 +46,15 @@ describe("GravatarService", () => {
     const rgx = new RegExp("^https://www.gravatar.com/avatar/(.*)$");
     expect(rgx.test(service.gravatarImageUrl)).toBeTrue();
   });
-  it("should get user account email addresses", async () => {
+  it("should get user email address", async () => {
     const responseStub = stub.AddressesHttpResponse(service.emailHash);
+    const httpShim = mockHttpShim(responseStub);
+    service.http = httpShim;
+    const result = await service.addresses();
+    expect(result.DidSucceed).toBeTrue();
+  });
+  it("should get user email addresses", async () => {
+    const responseStub = stub.AddressesMultipleHttpResponse(email, email2);
     const httpShim = mockHttpShim(responseStub);
     service.http = httpShim;
     const result = await service.addresses();
