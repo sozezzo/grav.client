3c1adf0b6d4c423fad5dbb3fcf030144da7d209a
mrtillman <askmrtillman@gmail.com>
2019-12-22 20:13:12 -0500

    stub grav.addresses xml

--- Common/TestDoubles/xml-response-stubs.ts
+++ Common/TestDoubles/xml-response-stubs.ts
@@ -1,5 +1,9 @@
 import { faultXmlResponse } from "../Assets/Xml/grav.fault.xml";
 import { existsXmlResponse } from "../Assets/Xml/grav.exists.xml";
+import { addressesXmlResponse } from "../Assets/Xml/grav.addresses.xml";
+import { addressesMultipleXmlResponse } from "../Assets/Xml/grav.addresses.multiple.xml";
 
 export const faultXml = faultXmlResponse;
 export const existsXml = existsXmlResponse;
+export const addressesXml = addressesXmlResponse;
+export const addressesMultipleXml = addressesMultipleXmlResponse;
