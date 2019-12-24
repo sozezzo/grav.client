fbf8deee0cfa79bf000e0f60d44d1afbbe0a88ea
mrtillman <askmrtillman@gmail.com>
2019-12-24 18:23:58 -0500

    replace xml strings

--- Common/TestDoubles/http-response-stubs.ts
+++ Common/TestDoubles/http-response-stubs.ts
@@ -1,15 +1,15 @@
 import { compile } from "handlebars";
-import * as xml from "../TestDoubles/xml-response-stubs";
-import { errorMessage } from "../TestDoubles/primitive-stubs";
+import * as stub from "../TestDoubles/xml-response-stubs";
 
-const faultResponse = compile(xml.faultXml);
-const existsResponse = compile(xml.existsXml);
-const addressesResponse = compile(xml.addressesXml);
-const addressesMultipleResponse = compile(xml.addressesMultipleXml);
-const useImageResponse = compile(xml.useUserImageXml);
-const useImageMultipleResponse = compile(xml.useUserImageMultipleXml);
-const removeImageResponse = compile(xml.removeImageXml);
-const removeImageMultipleResponse = compile(xml.removeImageMultipleXml);
+const faultResponse = compile(stub.faultXml);
+const existsResponse = compile(stub.existsXml);
+const existsMultipleResponse = compile(stub.existsMultipleXml);
+const addressesResponse = compile(stub.addressesXml);
+const addressesMultipleResponse = compile(stub.addressesMultipleXml);
+const useImageResponse = compile(stub.useUserImageXml);
+const useImageMultipleResponse = compile(stub.useUserImageMultipleXml);
+const removeImageResponse = compile(stub.removeImageXml);
+const removeImageMultipleResponse = compile(stub.removeImageMultipleXml);
 
 function errorResponse(errorMessage: string) {
   return faultResponse({ errorMessage });
@@ -40,6 +40,15 @@ export function ExistsHttpResponse(emailHash: string) {
   } as Response);
 }
 
+export function ExistsMultipleHttpResponse(emailHash: string, email2Hash: string) {
+  let xml: string = existsMultipleResponse({ emailHash, email2Hash });
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    text: () => Promise.resolve(xml)
+  } as Response);
+}
+
 export function AddressesHttpResponse(email: string) {
   let xml: string = addressesResponse({ email });
   return Promise.resolve({
@@ -59,164 +68,18 @@ export function AddressesMultipleHttpResponse(email1: string, email2: string) {
 }
 
 export function UserImagesHttpResponse() {
-  const xml = `
-  <?xml version="1.0"?>
-  <methodResponse>
-      <params>
-          <param>
-              <value>
-                  <struct>
-                      <member>
-                          <name>a0a78968780b5069869406cb34af326d</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/a0a78968780b5069869406cb34af326d.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>a68c2b2e469676717d9894c80ca16e82</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/a68c2b2e469676717d9894c80ca16e82.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>bdb24d1875ec87625e2da6f28605d800</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/bdb24d1875ec87625e2da6f28605d800.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>4ddf23534256fb555cfbf10acd7728b2</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/4ddf23534256fb555cfbf10acd7728b2.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>0fa6e24a27f544abb2536746b5b9d5f0</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/0fa6e24a27f544abb2536746b5b9d5f0.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>cd73b8e804398c2709453d36fba41ede</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/cd73b8e804398c2709453d36fba41ede.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>b823ea7d9a98e427c3cc21d38f7a17ed</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/b823ea7d9a98e427c3cc21d38f7a17ed.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                      <member>
-                          <name>d83bae7ec0e25069af6f3a006ecbf289</name>
-                          <value>
-                              <array>
-                                  <data>
-                                      <value>
-                                          <string>0</string>
-                                      </value>
-                                      <value>
-                                          <string>http://en.gravatar.com/userimage/150849239/d83bae7ec0e25069af6f3a006ecbf289.jpg</string>
-                                      </value>
-                                  </data>
-                              </array>
-                          </value>
-                      </member>
-                  </struct>
-              </value>
-          </param>
-      </params>
-  </methodResponse>
-`;
   return Promise.resolve({
     ok: true,
     status: 200,
-    text: () => Promise.resolve(xml)
+    text: () => Promise.resolve(stub.userImagesXml)
   } as Response);
 }
 
 export function SaveImageUrlHttpResponse() {
-  const xml = `
-  <?xml version="1.0"?>
-  <methodResponse>
-      <params>
-          <param>
-              <value>
-                  <string>b13ef59e996c16dcc127df002dd4578b</string>
-              </value>
-          </param>
-      </params>
-  </methodResponse>
-`;
-
   return Promise.resolve({
     ok: true,
     status: 200,
-    text: () => Promise.resolve(xml)
+    text: () => Promise.resolve(stub.saveUrlXml)
   } as Response);
 }
 
@@ -270,48 +133,17 @@ export function RemoveImageMultipleHttpResponse(
   } as Response);
 }
 export function DeleteUserImageHttpResponse() {
-  const xml = `
-    <?xml version="1.0"?>
-    <methodResponse>
-        <params>
-            <param>
-                <value>
-                    <boolean>1</boolean>
-                </value>
-            </param>
-        </params>
-    </methodResponse>
-  `;
   return Promise.resolve({
     ok: true,
     status: 200,
-    text: () => Promise.resolve(xml)
+    text: () => Promise.resolve(stub.deleteUserImageXml)
   } as Response);
 }
 
 export function TestHttpResponse() {
-  const xml = `
-    <?xml version="1.0"?>
-    <methodResponse>
-        <params>
-            <param>
-                <value>
-                    <struct>
-                        <member>
-                            <name>response</name>
-                            <value>
-                                <int>1555198365</int>
-                            </value>
-                        </member>
-                    </struct>
-                </value>
-            </param>
-        </params>
-    </methodResponse>
-  `;
   return Promise.resolve({
     ok: true,
     status: 200,
-    text: () => Promise.resolve(xml)
+    text: () => Promise.resolve(stub.testXml)
   } as Response);
 }
