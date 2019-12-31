4b6d1137e00f5974d5f6500029313fa76505b071
mrtillman <askmrtillman@gmail.com>
2019-12-30 20:17:51 -0500

    stub remove-image response

--- Common/TestDoubles/json-response-stubs.ts
+++ Common/TestDoubles/json-response-stubs.ts
@@ -4,6 +4,7 @@ import * as _addressesJsonResponse from "../Assets/Json/grav.addresses.json";
 import * as _userImagesJsonResponse from "../Assets/Json/grav.userImages.json";
 import * as _saveImageUrlResponse from "../Assets/Json/grav.saveUrl.json";
 import * as _useUserImageJsonResponse from "../Assets/Json/grav.useUserImage.json";
+import * as _removeImageJsonResponse from "../Assets/Json/grav.removeImage.json";
 import * as _testJsonResponse from "../Assets/Json/grav.test.json";
 
 export const existsJsonResponse = _existsJsonResponse;
@@ -12,4 +13,5 @@ export const addressesJsonResponse = _addressesJsonResponse;
 export const userImagesJsonResponse = _userImagesJsonResponse;
 export const saveImageUrlResponse = _saveImageUrlResponse;
 export const useUserImageJsonResponse = _useUserImageJsonResponse;
+export const removeImageJsonResponse = _removeImageJsonResponse;
 export const testJsonResponse = _testJsonResponse;
